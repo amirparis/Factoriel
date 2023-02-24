c43c9df2a216a591f6764ea89c9ae7bd1cb85f5e
Amirparsa Shakibaee <112088520+amirparis@users.noreply.github.com>
2023-02-24 17:36:12 +0330

    Add files via upload

---
+++ factorial-math.py
@@ -0,0 +1,4 @@
+import math
+number=int(input ( ' enter your number : '))
+result=math.factorial(number)
+print(result)
---
+++ factorial.py
@@ -0,0 +1,5 @@
+number=int(input(" Enter your number: "))
+factorial=1
+for i in range(1, number+1):
+    factorial=factorial*i
+print(factorial)
